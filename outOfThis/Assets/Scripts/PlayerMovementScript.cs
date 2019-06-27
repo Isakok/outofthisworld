675734b3620c4f5f1b27785e65cb673c2b55efc1
Isabelle <isabelle.wkpm@gmail.com>
2019-06-27 14:14:30 +0200

    Add horzontal movement + velocity

--- outOfThis/Assets/Scripts/PlayerMovementScript.cs
+++ outOfThis/Assets/Scripts/PlayerMovementScript.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class PlayerMovementScript : MonoBehaviour
 {
 
-    private Rigidbody2D rigidbody;
+    public float moveSpeed = 10.0f;
+    public float movement = 0f;
+    private Vector3 velocity = Vector3.zero;
+    public Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //walk
+        movement = Input.GetAxis("Horizontal");
+        Vector3 targetVelocity = new Vector2(movement * moveSpeed, rigidbody2d.velocity.y);
+        rigidbody2d.velocity = Vector3.SmoothDamp(rigidbody2d.velocity, targetVelocity, ref velocity, 0.05f);
+        //jump
     }
 }
