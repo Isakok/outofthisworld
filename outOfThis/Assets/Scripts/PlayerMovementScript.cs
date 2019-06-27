eb344fd1d748081e1dcc5cf69dd19f371f528728
Isabelle <isabelle.wkpm@gmail.com>
2019-06-27 13:57:16 +0200

    PMS add rigidbody

--- outOfThis/Assets/Scripts/PlayerMovementScript.cs
+++ outOfThis/Assets/Scripts/PlayerMovementScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+
+    private Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
